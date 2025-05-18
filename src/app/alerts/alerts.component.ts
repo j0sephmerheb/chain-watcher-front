d7e7f52d79ead6c26e860bcad7bdae48b54867e6
Joseph Merheb <joseph@merheb.net>
2025-05-18 13:53:57 +0200

    ADD : Chart

--- src/app/alerts/alerts.component.ts
+++ src/app/alerts/alerts.component.ts
@@ -5,22 +5,26 @@ import { AlertFormComponent } from './alert-form/alert-form.component';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { AlertsService } from '../services/alerts.service';
+import { PriceChartComponent } from './price-chart/price-chart.component';
+import { PricesService } from '../services/prices.service';
 
 
 @Component({
   selector: 'app-alerts',
   standalone: true,
-  imports: [AlertListComponent, AlertFormComponent],
+  imports: [AlertListComponent, AlertFormComponent, PriceChartComponent],
   templateUrl: './alerts.component.html',
 })
 export class AlertsComponent {
   alerts: any[] = [];
   selectedCoin = 'bitcoin'; // default
+  chartData: any = null;
 
   private alertsService = inject(AlertsService);
+  private pricesService = inject(PricesService);
 
   ngOnInit() {
-    this.loadAlertsForCoin(this.selectedCoin);
+    this.loadDataForCoin(this.selectedCoin);
   }
 
   onAlertCreated(newAlert: any) {
@@ -32,15 +36,26 @@ export class AlertsComponent {
   }
 
   onCoinChange(event: Event) {
-    const value = (event.target as HTMLSelectElement).value;
-    this.selectedCoin = value;
-    this.alertsService.setSelectedCoin(value);
-    this.loadAlertsForCoin(value);
+    const coin = (event.target as HTMLSelectElement).value;
+    this.selectedCoin = coin;
+    this.alertsService.setSelectedCoin(coin);
+    this.loadDataForCoin(coin);
   }
 
   loadAlertsForCoin(coin: string) {
     this.alertsService.getAlertsByCoin(coin).subscribe((data: any[]) => {
       this.alerts = data;
+    });
+  }
+
+  loadDataForCoin(coin: string) {
+    this.loadAlertsForCoin(coin);
+    this.loadChartForCoin(coin);
+  }
+
+  loadChartForCoin(coin: string) {
+    this.pricesService.getPriceData(coin).subscribe((data: any) => {
+      this.chartData = data;
     });
   }
 }
--- src/app/alerts/price-chart/price-chart.component.ts
+++ src/app/alerts/price-chart/price-chart.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-price-chart',
   standalone: true,
-  imports: [],
+  imports: [NgxChartsModule],
   templateUrl: './price-chart.component.html',
   styleUrl: './price-chart.component.scss'
 })
-export class PriceChartComponent {
+export class PriceChartComponent implements OnChanges {
+  @Input() prices: any[] = [];
+  @Input() first: number = 0;
+  @Input() last: number = 0;
+  @Input() percentage: number = 0;
+  @Input() direction: string = '';
+  @Input() coin: string = '';
 
+  chartSeries: any[] = [];
+  tickIndex = 0;
+
+  customColors = [
+    {
+      name: 'Price',
+      value: '#3b82f6', 
+    }
+  ];
+
+  formatXAxisTick = (val: string): string => {
+    const result = this.tickIndex % 6 === 0 ? val : '';
+    this.tickIndex++;
+    return result;
+  };
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['prices'] && this.prices?.length) {
+      this.chartSeries = [
+        {
+          name: 'Price',
+          series: this.prices.map((entry: any) => ({
+            name: entry.time,
+            value: entry.price,
+          })),
+        },
+      ];
+    }
+  }
 }
